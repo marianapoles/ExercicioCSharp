29f1e3242394969d5fcbbf4d6cb2c0cc501fe089
marianapoles <duda060510@gmail.com>
2026-04-22 14:34:35 -0300

    primeiro

---
+++ SaidaDeValoresTabuada/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tabuada do 9:");
+Console.WriteLine("9 * 1 = " (9 * 1));
+Console.WriteLine("9 * 1 = " (9 * 2));
+Console.WriteLine("9 * 1 = " (9 *3 ));
+Console.WriteLine("9 * 1 = " (9 * 4));
+Console.WriteLine("9 * 1 = " (9 * 5));
+Console.WriteLine("9 * 1 = " (9 * 6));
+Console.WriteLine("9 * 1 = " (9 * 7));
+Console.WriteLine("9 * 1 = " (9 * 8));
+Console.WriteLine("9 * 1 = " (9 * 9));
+Console.WriteLine("9 * 1 = " (9 * 10));
